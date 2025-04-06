57a9678a2cb7edf4128e25148c93e70070d17812
Pedro Manse <pedro.manse@dmk3.com.br>
2025-04-06 16:24:25 -0300

    multiple elements with jsx

--- src/jsx-example/script.tsx
+++ src/jsx-example/script.tsx
@@ -5,9 +5,14 @@ window.addEventListener("load", ()=>{
 	const [setPoint, getPoint] = ReactiveElement(
 		0,
 		$("#counter"),
-		point=>(<p>Points: {point}</p>)
+		point=>(<p>Points: {
+			...(new Array(point).fill(null).map((_,i)=>(<p>{i}</p>)))
+		}</p>)
 	);
 	$("#btn").addEventListener("click", ()=>{
 		setPoint(getPoint()+1);
+	});
+	$("#btn-1").addEventListener("click", ()=>{
+		setPoint(getPoint()-1);
 	});
 })
